89176309633364e80a713364e2de5934388f3ee7
Lindsey Handley <lindseyhandley@gmail.com>
2018-01-16 21:33:15 -0800

    current allocation and percentage and number formatting

--- check_asset_mix.R
+++ check_asset_mix.R
@@ -28,18 +28,36 @@ dolookup <- function(table){
 
 ##Before you pass file location in here, make sure the data is "clean". Check in TextWrangler.
 check_asset_mix <- function(file_location){
-  ##Read in current data from Vanguard and tidy
+  ##Read in current data from Vanguard and find asset allocation types
   investments <- read.csv(file_location)
   investments <- mutate(investments,assetType=sapply(Symbol,dolookup(symbol_assettype_lookup)))
+  
+  ##Tidy up column names
   names(investments) <- gsub("\\.","",names(investments))
   names(investments) <- tolower(names(investments))
+  names(investments) <- sub("totalvalue", "investmentallocation", names(investments))
   
-  #Group data by asset type and figure out current percentage investment vs. target
+  ##Group data by asset type and figure out current percentage investment vs. target
   asset_grouped_investments <- group_by(investments, assettype)
-  summary_table <- summarize(asset_grouped_investments, sum=sum(totalvalue))
-  total <- sum(summary_table$sum)
-  summary_table <- summarize(asset_grouped_investments, sum = sum(totalvalue), percent = (sum / total))
-  summary_table <- mutate(summary_table,target=sapply(assettype,dolookup(assettype_target_lookup)))
+  summary_table <- summarize(asset_grouped_investments, currentallocation=sum(investmentallocation))
+  total <- sum(summary_table$currentallocation)
+  summary_table <- mutate(summary_table, currentpercent = (currentallocation / total))
+  summary_table <- mutate(summary_table, target=sapply(assettype, dolookup(assettype_target_lookup)))
+  
+  ##Find perfect allocation and allocation difference
+  summary_table <- mutate(summary_table, perfectallocation = target * total)
+  summary_table <- mutate(summary_table, idealdifference = perfectallocation - currentallocation)
+  
+  ##Format columns appropriately
+  summary_table <- mutate(summary_table, currentallocation = sprintf("$%.2f",currentallocation))
+  summary_table <- mutate(summary_table, perfectallocation = sprintf("$%.2f",perfectallocation))
+  summary_table <- mutate(summary_table, idealdifference = sprintf("$%.2f",idealdifference))
+  summary_table <- mutate(summary_table, currentpercent = sprintf("%#.3f",currentpercent))
+  summary_table <- mutate(summary_table, target = sprintf("%#.3f",target))
   summary_table
 }
 
+
+##Figure out how to look up share prices for stock symbol
+##Recommend # of Shares to purchase to reach AllocationDifference (but less than)
+##Calculate the PostPurchaseAllocation && PostPurchasePercentage
